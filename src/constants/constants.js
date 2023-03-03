922b48c37e387b6861bccbcf76566e7e0dc50e2b
Kaipo <kaipojames12@gmail.com>
2023-03-03 00:41:12 -0600

    Implement DailyWeatherJob (Cron)

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,7 @@
 const Constants = {
   URL: {
+    BASE_URL: "https://weather-service-nnlr.onrender.com",
+    LOCAL_HOST: "localhost:3000",
     KMBC_ALERTS_URL: "https://www.kmbc.com/alerts",
     KMBC_WEATHER_URL: "https://www.kmbc.com/weather"
   },
@@ -9,6 +11,10 @@ const Constants = {
   MESSAGES: {
     NO_ALERTS_FOUND: 'No Severe Weather Alerts Found',
     ALERT_DETECTED: 'SEVER WEATHER ALERT DETECTED'
+  },
+  CRON: {
+    DAILY_AT_SEVEN_THIRTY: '30 7 * * *',
+    EVERY_MINUTE: '* * * * *'
   }
 }
 
