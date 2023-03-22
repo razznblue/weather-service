8a41cf0f71d8aac408709d500f119899a2fff56b
Kaipo <kaipojames12@gmail.com>
2023-03-22 14:46:20 -0500

    add isAdmin field on User Schema

--- src/schemas/UserSchema.js
+++ src/schemas/UserSchema.js
@@ -3,27 +3,13 @@ import mongoose from 'mongoose';
 const collectionName = "Users";
 const userSchema = new mongoose.Schema (
   {
-    username: {
-      type: String,
-      required: true
-    },
-    email: {
-      type: String,
-    },
-    phoneNumber: {
-      type: String,
-      required: true
-    },
-    registerDate: {
-      type: Date,
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    subscriptions: {
-      type: [{ type: String }]
-    }
+    username: { type: String, required: true },
+    email: { type: String },
+    phoneNumber: { type: String, required: true },
+    registerDate: { type: Date },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+    subscriptions: { type: [{ type: String }] }
   }, { timestamps: true, versionKey: false, collection: collectionName }
 );
 
